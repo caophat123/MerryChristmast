db530c0707ce12d369ae075d3bf757d9d4918b58
Phát Cao <caophat.2018@gmail.com>
2024-12-15 21:59:46 +0700

    change

--- DemoWeb/App_Start/RouteConfig.cs
+++ DemoWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DemoWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "TrangChu", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
             );
         }
     }
